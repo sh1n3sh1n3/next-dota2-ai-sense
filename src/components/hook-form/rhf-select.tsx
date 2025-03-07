11cb80fcc0c561c10293daca8dde0f5b39a53653
sh1n3sh1n3 <106221243+sh1n3sh1n3@users.noreply.github.com>
2025-03-08 02:19:45 +0800

    chore: fixed eslint issue

--- src/components/hook-form/rhf-select.tsx
+++ src/components/hook-form/rhf-select.tsx
@@ -82,84 +82,84 @@ type RHFMultiSelectProps = SelectProps & {
   }[];
 };
 
-export function RHFMultiSelect({
-  name,
-  chip,
-  label,
-  options,
-  checkbox,
-  placeholder,
-  helperText,
-  sx,
-  ...other
-}: RHFMultiSelectProps) {
-  const { control } = useFormContext();
+// export function RHFMultiSelect({
+//   name,
+//   chip,
+//   label,
+//   options,
+//   checkbox,
+//   placeholder,
+//   helperText,
+//   sx,
+//   ...other
+// }: RHFMultiSelectProps) {
+//   const { control } = useFormContext();
 
-  const renderValues = (selectedIds: string[]) => {
-    const selectedItems = options.filter((item) => selectedIds.includes(item.value));
+//   const renderValues = (selectedIds: string[]) => {
+//     const selectedItems = options.filter((item) => selectedIds.includes(item.value));
 
-    if (!selectedItems.length && placeholder) {
-      return (
-        <Box component="em" sx={{ color: 'text.disabled' }}>
-          {placeholder}
-        </Box>
-      );
-    }
+//     if (!selectedItems.length && placeholder) {
+//       return (
+//         <Box component="em" sx={{ color: 'text.disabled' }}>
+//           {placeholder}
+//         </Box>
+//       );
+//     }
 
-    if (chip) {
-      return (
-        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-          {selectedItems.map((item) => (
-            <Chip key={item.value} size="small" label={item.label} />
-          ))}
-        </Box>
-      );
-    }
+//     if (chip) {
+//       return (
+//         <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+//           {selectedItems.map((item) => (
+//             <Chip key={item.value} size="small" label={item.label} />
+//           ))}
+//         </Box>
+//       );
+//     }
 
-    return selectedItems.map((item) => item.label).join(', ');
-  };
+//     return selectedItems.map((item) => item.label).join(', ');
+//   };
 
-  return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field, fieldState: { error } }) => (
-        <FormControl sx={sx}>
-          {label && <InputLabel id={name}> {label} </InputLabel>}
+//   return (
+//     <Controller
+//       name={name}
+//       control={control}
+//       render={({ field, fieldState: { error } }) => (
+//         <FormControl sx={sx}>
+//           {label && <InputLabel id={name}> {label} </InputLabel>}
 
-          <Select
-            {...field}
-            multiple
-            displayEmpty={!!placeholder}
-            labelId={name}
-            input={<OutlinedInput fullWidth label={label} error={!!error} />}
-            renderValue={renderValues}
-            {...other}
-          >
-            {placeholder && (
-              <MenuItem disabled value="">
-                <em> {placeholder} </em>
-              </MenuItem>
-            )}
+//           <Select
+//             {...field}
+//             multiple
+//             displayEmpty={!!placeholder}
+//             labelId={name}
+//             input={<OutlinedInput fullWidth label={label} error={!!error} />}
+//             renderValue={renderValues}
+//             {...other}
+//           >
+//             {placeholder && (
+//               <MenuItem disabled value="">
+//                 <em> {placeholder} </em>
+//               </MenuItem>
+//             )}
 
-            {options.map((option) => {
-              const selected = field.value.includes(option.value);
+//             {options.map((option) => {
+//               const selected = field.value.includes(option.value);
 
-              return (
-                <MenuItem key={option.value} value={option.value}>
-                  {checkbox && <Checkbox size="small" disableRipple checked={selected} />}
+//               return (
+//                 <MenuItem key={option.value} value={option.value}>
+//                   {checkbox && <Checkbox size="small" disableRipple checked={selected} />}
 
-                  {option.label}
-                </MenuItem>
-              );
-            })}
-          </Select>
+//                   {option.label}
+//                 </MenuItem>
+//               );
+//             })}
+//           </Select>
 
-          {(!!error || helperText) && (
-            <FormHelperText error={!!error}>{error ? error?.message : helperText}</FormHelperText>
-          )}
-        </FormControl>
-      )}
-    />
-  );
-}
+//           {(!!error || helperText) && (
+//             <FormHelperText error={!!error}>{error ? error?.message : helperText}</FormHelperText>
+//           )}
+//         </FormControl>
+//       )}
+//     />
+//   );
+// }
